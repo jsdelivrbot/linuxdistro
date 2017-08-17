7aa06df040deccbc7b31f71501524320fc2080db
Laboratório de Redes <andre.antonitsch@acad.pucrs.br>
2017-08-17 13:26:20 -0300

    automatic server configuration on build

--- buildroot/custom-scripts/pre-build.sh
+++ buildroot/custom-scripts/pre-build.sh
@@ -3,5 +3,5 @@
 cp $BASE_DIR/../custom-scripts/S41network-config $BASE_DIR/target/etc/init.d
 chmod +x $BASE_DIR/target/etc/init.d/S41network-config
 
-cp $BASE_DIR/../custom-scripts/server.py $BASE_DIR/target/scripts/server.py
-chmod +x $BASE_DIR/target/scripts/server.py
+cp $BASE_DIR/../custom-scripts/serverscripts $BASE_DIR/target/serverscripts
+chmod +x $BASE_DIR/target/serverscripts
