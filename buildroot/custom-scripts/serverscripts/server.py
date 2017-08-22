0228ec0b5722295574450bd6142117f09328d723
Laboratório de Redes <andre.antonitsch@acad.pucrs.br>
2017-08-22 13:19:50 -0300

    server modifications

--- buildroot/custom-scripts/serverscripts/server.py
+++ buildroot/custom-scripts/serverscripts/server.py
@@ -5,7 +5,7 @@ import psutil
 import platform
 import os.path
 import sys
-
+import re
 
 class MyRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -15,23 +15,29 @@ class MyRequestHandler(BaseHTTPRequestHandler):
         cpu_usage = psutils.cpu_percent()
         memory_usage = psutil.virtual_memory()
         
-        process_list = []
-
+        process_list = ''
+    
         for process in psutil.process_iter():
             try:
                 process_info = process.as_dict(attrs=['pid', 'name'])
-                process_list.append(process_info)
+                process_list += "PID " + process_info['pid'] + " " + "NAME " + process_info['name'] + "\n"
             except psutil.NoSuchProcess:
                 pass
-        
-
-
 
         self.path = '/index.html'
+        f = open(self.path)
+        re.sub("<H1>Process List</H1>(.+)</P>","<P>"+process_list  , f)
+        re.sub("<H1>System Time</H1>(.+)</P>","<P>"+current_time  , f)
+        re.sub("<H1>System Uptime</H1>(.+)</P>","<P>"+elapsed_time  , f)
+        re.sub("<H1>Processor Model</H1>(.+)</P>","<P>"+processor  , f)
+        re.sub("<H1>Processor Usage</H1>(.+)</P>","<P>"+cpu_usage  , f)
+        re.sub("<H1>Total Ram</H1>(.+)</P>","<P>"+memory_usage  , f)
+        re.sub("<H1>System Version</H1>(.+)</P>","<P>"+linux  , f)
+
         return BaseHTTPRequestHandler.do_GET(self)
 
 
-
+re.DOTALL = True
 server_start = time.time()
 processor = platform.processor()
 linux = platform.linux_distribution()
