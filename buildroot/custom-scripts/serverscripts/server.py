fbe2cf814f3a4dd572e28e82dace16b5fe121a35
Laboratório de Redes <andre.antonitsch@acad.pucrs.br>
2017-08-29 13:11:05 -0300

    webserver finished

--- buildroot/custom-scripts/serverscripts/server.py
+++ buildroot/custom-scripts/serverscripts/server.py
@@ -1,4 +1,4 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
+from http.server import HTTPServer, SimpleHTTPRequestHandler, BaseHTTPRequestHandler
 from socketserver import TCPServer
 import time
 import psutil
@@ -7,11 +7,20 @@ import os.path
 import sys
 import re
 
-class MyRequestHandler(BaseHTTPRequestHandler):
+server_start = time.time()
+processor = platform.processor()
+linux = platform.linux_distribution()[0] + " " + platform.linux_distribution()[1]
+
+
+class MyRequestHandler(SimpleHTTPRequestHandler):
     def do_GET(self):
         
         current_time = time.localtime()
-        elapsed_time = time.localtime(time.time() - server_start)
+        elapsed_time = time.time() - server_start
+
+        string_current_t = time.strftime("%a, %d %b %Y %H:%M:%S", current_time)
+        #string_elapsed_t = time.strftime("%a, %d %b %Y %H:%M:%S", elapsed_time)
+
         cpu_usage = psutil.cpu_percent()
         memory_usage = psutil.virtual_memory()
         
@@ -20,32 +29,31 @@ class MyRequestHandler(BaseHTTPRequestHandler):
         for process in psutil.process_iter():
             try:
                 process_info = process.as_dict(attrs=['pid', 'name'])
-                process_list += "PID " + str(process_info['pid']) + " " + "NAME " + process_info['name'] + "\n"
+                process_list += "PID " + str(process_info['pid']) + " " + "NAME " + process_info['name'] + "\n    "
             except psutil.NoSuchProcess:
                 pass
-        self.path = '/index.html'
-        index_file = open('.'+self.path)
+        path = '/usr/bin/serverscripts/index_bkp.html'
+        index_file = open(path)
         f = index_file.read()
-        re.sub("<H1>Process List</H1>(.+)</P>","<P>"+ process_list  , f)
-        re.sub("<H1>System Time</H1>(.+)</P>","<P>"+ current_time  , f)
-        re.sub("<H1>System Uptime</H1>(.+)</P>","<P>"+elapsed_time  , f)
-        re.sub("<H1>Processor Model</H1>(.+)</P>","<P>"+processor  , f)
-        re.sub("<H1>Processor Usage</H1>(.+)</P>","<P>"+cpu_usage  , f)
-        re.sub("<H1>Total Ram</H1>(.+)</P>","<P>"+memory_usage  , f)
-        re.sub("<H1>System Version</H1>(.+)</P>","<P>"+linux  , f)
-        
         index_file.close()
-        index_file = open('.'+self.path, 'w')
+
+        f = re.sub(r"hhh", process_list, f)
+        f = re.sub(r"aaa", string_current_t, f)
+        f = re.sub(r"bbb", str(elapsed_time), f)
+        f = re.sub(r"ccc", processor, f)
+        f = re.sub(r"ddd", str(cpu_usage), f)
+        f = re.sub(r"eee", str(memory_usage), f)
+        f = re.sub(r"ggg", linux, f)
+
+        self.path = '/usr/bin/serverscripts/index.html'
+        index_file = open(self.path, 'w')
         index_file.write(f)
         index_file.close()
+
         print("connection recieved")
 
-        return BaseHTTPRequestHandler.do_GET(self)
+        return SimpleHTTPRequestHandler.do_GET(self)
 
-re.DOTALL = True
-server_start = time.time()
-processor = platform.processor()
-linux = platform.linux_distribution()
 
 Handler = MyRequestHandler
 server = TCPServer(('0.0.0.0', 8094), Handler)
